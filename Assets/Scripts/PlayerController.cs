48f4c53815a82a5d888476c733872b588f3b921e
Lunecron <Lunecron@live.de>
2021-11-18 21:49:39 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 3f;
+    [SerializeField] float torqueAmount = 10f;
 
     Rigidbody2D rb2d;
     
